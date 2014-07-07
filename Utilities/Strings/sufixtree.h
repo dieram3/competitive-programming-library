dad6a8b2709b3dc49b820402340d94e43b9a28a7
Patricio Beckmann <pato.beckmann@gmail.com>
2014-07-07 02:33:29 -0400

    SufixTree for Utilities

---
+++ Utilities/Strings/sufixtree.h
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cstdio>
+#include <map>
+#include <string>
+ 
+ 
+class SuffixTree {
+  public:
+    struct Node {
+      std::map<char, Node> children;
+      std::size_t count;
+    };
+ 
+  public:
+    explicit SuffixTree(const std::string &str) {
+      auto first = str.begin();
+      auto last  = str.end();
+      for (; first != last; ++first) {
+        InsertSuffix(first, last);
+      }
+    }
+    std::string LongestRepeatedSubStr() const {
+      std::string longestSubStr;
+      std::string currentSubStr;
+      LongestRepeatedSubStrImpl(longestSubStr, currentSubStr, root_);
+      return longestSubStr;
+    }
+ 
+  private:
+    void LongestRepeatedSubStrImpl(std::string &LongestStr,
+                                   std::string &currentStr,
+                                   const Node &currentNode) const {
+      if (currentNode.count > 1 &&  currentStr.size() > LongestStr.size()) {
+        LongestStr = currentStr;
+      }
+      for (const auto &p : currentNode.children) {
+        currentStr.push_back(p.first);
+        LongestRepeatedSubStrImpl(LongestStr, currentStr, p.second);
+        currentStr.pop_back();
+      }
+    }
+ 
+    template<class Iterator>
+    void InsertSuffix(const Iterator first, const Iterator last) {
+      auto current  = &root_;
+      std::for_each(first, last, [&current](char c) {
+        current = &current->children[c];
+        ++current->count;
+      });
+    }
+ 
+  private:
+    Node root_;
+};
