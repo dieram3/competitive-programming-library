db5c0a915724e2c527b7397107417586953aefd8
Diego Ramirez <diego_rd93@hotmail.com>
2014-10-28 23:51:46 -0300

    Modified segment tree

--- include/djp/container/segment_tree.hpp
+++ include/djp/container/segment_tree.hpp
@@ -7,6 +7,7 @@
 #define DJP_CONTAINER_SEGMENT_TREE_HPP
 
 #include <vector>
+#include <iterator>
 #include <utility>
 #include <cassert>
 
@@ -17,50 +18,49 @@ class segment_tree {
  public:
   using value_type = T;
   using size_type = std::size_t;
+  using pos_type = std::size_t;
 
  private:
-  using range_t = std::pair<size_type, size_type>;
+  using range_t = std::pair<pos_type, pos_type>;
 
   struct node {
-    node(size_type b, size_type e, size_type p)
-        : begin{b}, end{e}, middle{b + (e - b) / 2}, pos{p} {}
+    node(pos_type b, pos_type e, size_type tp)
+        : begin{b}, end{e}, tree_pos{tp} {}
 
     bool is_leaf() const { return begin + 1 == end; }
+    pos_type middle() const { return begin + (end - begin) / 2; }
+    node left() const { return {begin, middle(), 2 * tree_pos + 1}; }
+    node right() const { return {middle(), end, 2 * tree_pos + 2}; }
 
-    node left() const { return {begin, middle, 2 * pos + 1}; }
-
-    node right() const { return {middle, end, 2 * pos + 2}; }
-
-    size_type begin;
-    size_type end;
-    size_type middle;
-    size_type pos;
+    pos_type begin;
+    pos_type end;
+    size_type tree_pos;
   };
 
  public:
-  /// Constructs the segment tree with the elements in the range [first, last)
-  /// using the associative operator op.
-  /// Complexity: (last - first) applications of op.
+  /// Constructs the segment tree with the elements in
+  /// the range [first, last) using the associative binary operator op.
+  /// Complexity: (last - first) applications of op
   template <class ForwardIt>
   segment_tree(ForwardIt first, ForwardIt last, BinaryOp op = BinaryOp())
       : size_(std::distance(first, last)),
         op_(op),
         tree_(optimal_tree_size(size_)) {
-    auto take_element = [&first](T &value) { value = *first++; };
+    auto take_element = [&first](T& value) { value = *first++; };
     for_each(0, size(), take_element);
   }
 
-  /// Computes the sum of the elements in the range [first, last)
-  /// Complexity: log(N) applications of op, where N == size()
-  T accumulate(size_type first, size_type last) const {
-    return accumulate(root(), range_t(first, last));
+  // Computes the sum of the elements in the range [first, last)
+  // Complexity: log(N) applications of op, where N == size()
+  T accumulate(pos_type first, pos_type last) const {
+    return accumulate(root(), first, last);
   }
 
-  /// Applies the given function f to each element in the range [first, last)
-  /// Complexity: max(log(N), 2*k) aplications of op and k applications of f
-  /// where N == size() and k == last - first
+  // Applies the given function f to each element in the range [first, last)
+  // Complexity: max(log(N), 2*k) aplications of op and k applications of f
+  // where N == size() and k == last - first
   template <class UnaryFunction>
-  void for_each(size_type first, size_type last, UnaryFunction f) {
+  void for_each(pos_type first, pos_type last, UnaryFunction f) {
     for_each(root(), range_t(first, last), f);
   }
 
@@ -71,9 +71,9 @@ class segment_tree {
  private:
   node root() const { return {0, size(), 0}; }
 
-  T &elem(const node &nd) { return tree_[nd.pos]; }
+  T& elem(const node& nd) { return tree_[nd.tree_pos]; }
 
-  const T &elem(const node &nd) const { return tree_[nd.pos]; }
+  const T& elem(const node& nd) const { return tree_[nd.tree_pos]; }
 
   static size_type optimal_tree_size(size_type sz) {
     if (!sz) return 0;
@@ -82,27 +82,28 @@ class segment_tree {
     return res - 1;
   }
 
-  T accumulate(const node &nd, const range_t &target) const {
-    if (nd.begin >= target.first && nd.end <= target.second) return elem(nd);
+  T accumulate(const node& nd, pos_type first, pos_type last) const {
+    if (nd.begin == first && nd.end == last) return elem(nd);
     assert(!nd.is_leaf());
 
-    if (target.second <= nd.middle) return accumulate(nd.left(), target);
-    if (target.first >= nd.middle) return accumulate(nd.right(), target);
+    if (last <= nd.middle()) return accumulate(nd.left(), first, last);
+    if (first >= nd.middle()) return accumulate(nd.right(), first, last);
 
-    const auto lsum = accumulate(nd.left(), target);
-    const auto rsum = accumulate(nd.right(), target);
+    const auto lsum = accumulate(nd.left(), first, nd.middle());
+    const auto rsum = accumulate(nd.right(), nd.middle(), last);
     return op_(lsum, rsum);
   }
 
   template <class UnaryFunc>
-  void for_each(const node &nd, const range_t &target, UnaryFunc &f) {
-    if (nd.begin >= target.second || nd.end <= target.first) return;
+  void for_each(const node& nd, const range_t& target, UnaryFunc& f) {
     if (nd.is_leaf()) return f(elem(nd));
 
     const auto lchild = nd.left();
     const auto rchild = nd.right();
-    for_each(lchild, target, f);
-    for_each(rchild, target, f);
+
+    if (target.first < nd.middle()) for_each(lchild, target, f);
+    if (target.second > nd.middle()) for_each(rchild, target, f);
+
     elem(nd) = op_(elem(lchild), elem(rchild));
   }
 
