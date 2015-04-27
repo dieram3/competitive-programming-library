de6095cda0311ca3357ad08799df64cd64ca11a9
Diego Ramírez <diego_rd93@hotmail.com>
2015-04-26 23:00:41 -0300

    basics has been formatted

--- include/djp/strings/basics.hpp
+++ include/djp/strings/basics.hpp
@@ -4,23 +4,23 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <djp/string/string_view.hpp>
+#include <djp/strings/string_view.hpp>
 
 namespace djp {
 
 std::vector<string_view> split(const std::string& str, const std::string& sep) {
-    std::vector<string_view> tokens;
-    auto pos = str.find_first_not_of(sep);
-    while(pos != std::string::npos) {
-        auto end_pos = str.find_first_of(sep, pos+1);
-        end_pos = std::min(end_pos, str.size());
-        tokens.emplace_back(str, pos, end_pos - pos);
-        if(end_pos == str.size()) break;
-        pos = str.find_first_not_of(sep, end_pos+1);
-    }
-    return tokens;
+  std::vector<string_view> tokens;
+  auto pos = str.find_first_not_of(sep);
+  while (pos != std::string::npos) {
+    auto end_pos = str.find_first_of(sep, pos + 1);
+    end_pos = std::min(end_pos, str.size());
+    tokens.emplace_back(str, pos, end_pos - pos);
+    if (end_pos == str.size()) break;
+    pos = str.find_first_not_of(sep, end_pos + 1);
+  }
+  return tokens;
 }
 
-} // namespace djp
+}  // namespace djp
 
-#endif // Header guard
+#endif  // Header guard
