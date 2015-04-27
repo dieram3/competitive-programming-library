80935d2f78b306b3789f36a14e5247e9d230cf43
Diego Ramírez <diego_rd93@hotmail.com>
2015-04-27 19:45:51 -0300

    Improved suffix array example

--- example/suffix_array_search.cpp
+++ example/suffix_array_search.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <djp/strings/suffix_array.hpp>
 
-struct comparer {
+struct compare {
   const std::string* text;
   bool operator()(size_t suffix, const std::string& pattern) const {
     return text->compare(suffix, pattern.size(), pattern) < 0;
@@ -20,21 +20,18 @@ int main(int argc, char* argv[]) {
   }
 
   std::string text = "The quick brown fox jumps over the lazy dog";
-  std::string pattern = argv[1];
-  const auto sa = djp::make_suffix_array(text);
-
   std::cout << "Text: \"" << text << "\"\n\n";
+  auto sa = djp::make_suffix_array(text);
 
-  auto p = std::equal_range(sa.begin(), sa.end(), pattern, comparer{&text});
-  size_t appearances = p.second - p.first;
+  std::string pattern = argv[1];
+  auto p = std::equal_range(sa.begin(), sa.end(), pattern, compare{&text});
+  auto appearances = std::distance(p.first, p.second);
   if (appearances == 0) {
     std::cout << "Pattern not found" << '\n';
   } else {
     std::cout << "Pattern found " << appearances << " times:\n";
-    for (; p.first != p.second; ++p.first) {
-      size_t suffix = *p.first;
-      std::cout << "Suffix(" << suffix << "): \"" << text.substr(suffix, 10)
-                << "...\"\n";
-    }
+    std::for_each(p.first, p.second, [&text](size_t pos) {
+      std::cout << "At " << pos << ": \"" << text.substr(pos, 10) << "...\"\n";
+    });
   }
 }
