e87d1d1d4c07c56a6ceaf35d550071ff0d92e058
Diego Ramírez <diego_rd93@hotmail.com>
2015-05-30 03:19:03 -0300

    Minor comments corrections

--- include/djp/number_theory/common_factor.hpp
+++ include/djp/number_theory/common_factor.hpp
@@ -3,23 +3,21 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef DJP_MATH_COMMON_FACTOR_HPP
-#define DJP_MATH_COMMON_FACTOR_HPP
+#ifndef DJP_NUMBER_THEORY_COMMON_FACTOR_HPP
+#define DJP_NUMBER_THEORY_COMMON_FACTOR_HPP
 
 namespace djp {
 
 /// Computes the greatest common divisor between  two integer numbers.
-template <class T>
-constexpr T gcd(T a, T b) {
+template <class T> constexpr T gcd(T a, T b) {
   return b == 0 ? a : gcd(b, a % b);
 }
 
 /// Computes the least common multiple between two integer numbers.
-template <class T>
-constexpr T lcm(T a, T b) {
+template <class T> constexpr T lcm(T a, T b) {
   return a || b ? a / gcd(a, b) * b : 0;
 }
 
-}  // namespace djp
+} // namespace djp
 
-#endif  // Header guard
+#endif // Header guard
--- include/djp/number_theory/primes.hpp
+++ include/djp/number_theory/primes.hpp
@@ -1,3 +1,8 @@
+//          Copyright Diego Ramírez May 2015
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef DJP_NUMBER_THEORY_PRIMES_HPP
 #define DJP_NUMBER_THEORY_PRIMES_HPP
 
