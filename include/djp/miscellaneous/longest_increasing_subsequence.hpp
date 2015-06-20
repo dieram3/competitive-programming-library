917cfaeabdebc4e56cb3f5c21a77e88b8b4c5185
Diego Ramírez <diego_rd93@hotmail.com>
2015-06-20 03:10:41 -0300

    Fixed calc_lis_size Doxygen comments

--- include/djp/miscellaneous/longest_increasing_subsequence.hpp
+++ include/djp/miscellaneous/longest_increasing_subsequence.hpp
@@ -2,6 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
+/// \file
+/// \brief Defines functions related to the longest increasing subsequence.
 
 #ifndef DJP_MISCELLANEOUS_LONGEST_INCREASING_SUBSEQUENCE_HPP
 #define DJP_MISCELLANEOUS_LONGEST_INCREASING_SUBSEQUENCE_HPP
@@ -13,12 +15,19 @@
 
 namespace djp {
 
-/// \brief Calculates size of the Longest Monotonically Increasing Subsequence.
-/// Complexity: O(N*log(N)) comparisons, where N == std::distance(first, last)
-/// comp(lhs, rhs) must returns true if lhs is considered strictly less than
-/// rhs
-/// \note: If you need a longest increasing that is not Monotonically Increasing
-/// you can use a <= or >= compare
+/// \brief Calculates size of the Longest monotonically Increasing Subsequence.
+///
+/// The \e LIS is defined as the longest subsequence of <tt>[first, last)</tt>
+/// such that <tt>comp(LIS[i], LIS[i + 1])</tt> evaluates to \c true for all
+/// <tt>LIS[i], LIS[i +1]</tt> which are valid references.
+///
+/// \param first Iterator to the beginning of the target range.
+/// \param last Iterator to the end of the target range.
+/// \param comp Comparison function object which returns \c true if the first
+/// argument is \em less than the second.
+/// \par Complexity
+/// <tt>O(N * log(N))</tt> comparisons, where <tt>N = std::distance(first,
+/// last)</tt>.
 template <class InputIt, class Compare>
 std::size_t calc_lis_size(InputIt first, InputIt last, Compare comp) {
   using T = typename std::iterator_traits<InputIt>::value_type;
@@ -34,12 +43,15 @@ std::size_t calc_lis_size(InputIt first, InputIt last, Compare comp) {
   return tail.size();
 }
 
+/// \brief Calculates size of the Longest Monotonically Increasing Subsequence.
+///
+/// Calls <tt>calc_lis_size(first, last, std::less<>())</tt>.
 template <class InputIt>
 std::size_t calc_lis_size(InputIt first, InputIt last) {
   using T = typename std::iterator_traits<InputIt>::value_type;
   return calc_lis_size(first, last, std::less<T>());
 }
 
-} // djp
+} // end namespace djp
 
 #endif // Header guard
