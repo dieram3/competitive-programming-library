3e915d2925c1fd226747df17e6501bb287c21bd7
Diego Ramírez <diego_rd93@hotmail.com>
2015-08-12 00:51:48 -0300

    Improved SCC function

--- include/djp/graph/strong_components.hpp
+++ include/djp/graph/strong_components.hpp
@@ -6,22 +6,21 @@
 #ifndef DJP_GRAPH_STRONG_COMPONENTS_HPP
 #define DJP_GRAPH_STRONG_COMPONENTS_HPP
 
-#include <stack>   // for std::stack
-#include <utility> // for std::pair
-#include <vector>  // for std::vector
-#include <cstddef> // for std::size_t
-#include <cstdint> // for SIZE_MAX
-
-#include <iostream> // TEMPORAL
+#include <algorithm>  // for std::min
+#include <functional> // for std::function
+#include <stack>      // for std::stack
+#include <vector>     // for std::vector
+#include <cstddef>    // for std::size_t
+#include <cstdint>    // for SIZE_MAX
 
 namespace djp {
 
 /// \brief Finds the strongly connected components (SCC) of a directed graph.
 ///
-/// Uses an iterative version of Tarjan's algorithm based on DFS to find the SCC
-/// of the input graph. The component labels are recorded in the \p comp map.
-/// Label values will be bounded into the range <tt>[0, C)</tt>, being \c C the
-/// total number of strongly connected components.
+/// Uses the Tarjan's algorithm based to find the SCC of the input graph. The
+/// component labels are recorded in the \p comp map. Label values will be
+/// bounded into the range <tt>[0, C)</tt>, being \c C the total number of
+/// strongly connected components.
 ///
 /// \param g The target graph.
 /// \param[out] comp The component map.
@@ -35,26 +34,26 @@ template <typename Graph>
 size_t strong_components(const Graph &g, std::vector<size_t> &comp) {
 
   const size_t num_vertices = g.num_vertices();
-  std::stack<size_t> s;
-  std::vector<size_t> root(num_vertices);
-  std::vector<size_t> dfs_time(num_vertices);
   size_t time = 0;
   size_t comp_cnt = 0;
+  std::stack<size_t> s;
+  std::vector<size_t> low(num_vertices);
+  std::vector<size_t> dtm(num_vertices);
+  comp.resize(num_vertices);
 
-  auto discover = [&](const size_t v) {
-    root[v] = v;
+  std::function<void(size_t)> dfs_visit;
+  dfs_visit = [&](const size_t v) {
+    low[v] = dtm[v] = ++time;
     comp[v] = SIZE_MAX;
     s.push(v);
-    dfs_time[v] = ++time;
-  };
-
-  auto finish = [&](const size_t v) {
     for (const auto edge : g.out_edges(v)) {
-      const auto w = g.target(edge);
-      if (comp[w] == SIZE_MAX && dfs_time[root[w]] < dfs_time[root[v]])
-        root[v] = root[w];
+      const size_t w = g.target(edge);
+      if (!dtm[w])
+        dfs_visit(w);
+      if (comp[w] == SIZE_MAX)
+        low[v] = std::min(low[v], low[w]);
     }
-    if (v != root[v])
+    if (dtm[v] != low[v])
       return;
     size_t w;
     do {
@@ -64,28 +63,8 @@ size_t strong_components(const Graph &g, std::vector<size_t> &comp) {
     ++comp_cnt;
   };
 
-  auto dfs_visit = [&](const size_t start) {
-    std::stack<std::pair<size_t, bool>> dfs_stack;
-    dfs_stack.emplace(start, false);
-    while (!dfs_stack.empty()) {
-      const size_t u = dfs_stack.top().first;
-      const bool backtrack = dfs_stack.top().second;
-      dfs_stack.pop();
-      if (!backtrack) {
-        if (dfs_time[u])
-          continue; // Non-tree edge.
-        discover(u);
-        dfs_stack.emplace(u, true);
-        for (const auto edge : g.out_edges(u))
-          dfs_stack.emplace(g.target(edge), false);
-      } else
-        finish(u);
-    }
-  };
-
-  comp.resize(num_vertices);
   for (size_t v = 0; v != num_vertices; ++v)
-    if (!dfs_time[v])
+    if (!dtm[v])
       dfs_visit(v);
   return comp_cnt;
 }
