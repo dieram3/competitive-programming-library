e38b20f0d23303494a725b0f38e1d50f8f4710c8
Diego Ramírez <diego_rd93@hotmail.com>
2015-08-14 02:39:23 -0300

    Changed toposort to DFS based version. It does not requires in edges, is a bit more faster and it is shorter to code

--- include/djp/graph/topological_sort.hpp
+++ include/djp/graph/topological_sort.hpp
@@ -6,10 +6,10 @@
 #ifndef DJP_GRAPH_TOPOLOGICAL_SORT_HPP
 #define DJP_GRAPH_TOPOLOGICAL_SORT_HPP
 
-#include <stdexcept> // for std::logical_error
-#include <vector>    // for std::vector
-#include <cstddef>   // for std::size_t
-#include <cassert>   // for assert
+#include <functional> // for std::function
+#include <stdexcept>  // for std::logic_error
+#include <vector>     // for std::vector
+#include <cstddef>    // for std::size_t
 
 namespace djp {
 
@@ -27,38 +27,31 @@ namespace djp {
 ///
 template <typename Graph>
 std::vector<size_t> topological_sort(const Graph &g) {
+  enum colors { white, gray, black };
+  const size_t num_v = g.num_vertices();
+  std::vector<size_t> list(num_v);
+  std::vector<colors> color(num_v, white);
+  size_t cur_pos = num_v;
 
-  const size_t num_vertices = g.num_vertices();
-  std::vector<size_t> sorted_list;
-  sorted_list.reserve(num_vertices);
-
-  std::vector<size_t> in_degree(num_vertices);
-
-  for (size_t v = 0; v != num_vertices; ++v) {
-    in_degree[v] = g.in_degree(v);
-    if (in_degree[v] == 0)
-      sorted_list.push_back(v);
-  }
-
-  size_t num_processed = 0;
-  while (num_processed < sorted_list.size()) {
-    // Note: If sorted_list.size() - num_processed > 1 in some cycle, there are
-    // multiple topological sorts.
-
-    const size_t root = sorted_list[num_processed++];
-
-    for (const auto edge : g.out_edges(root)) {
-      const size_t child = g.target(edge);
-      --in_degree[child];
-      if (in_degree[child] == 0)
-        sorted_list.push_back(child);
+  std::function<void(size_t)> dfs_visit;
+  dfs_visit = [&](const size_t src) {
+    color[src] = gray;
+    for (const auto e : g.out_edges(src)) {
+      const size_t tgt = g.target(e);
+      if (color[tgt] == white)
+        dfs_visit(tgt);
+      else if (color[tgt] == gray)
+        throw std::logic_error("Not a DAG");
     }
-  }
+    color[src] = black;
+    list[--cur_pos] = src;
+  };
 
-  if (num_processed < num_vertices)
-    throw std::logic_error("topological_sort: Not a DAG");
+  for (size_t v = 0; v != num_v; ++v)
+    if (color[v] == white)
+      dfs_visit(v);
 
-  return sorted_list;
+  return list;
 }
 
 } // end namespace djp
