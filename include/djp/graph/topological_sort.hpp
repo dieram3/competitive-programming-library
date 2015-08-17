206ae441488a073fbf8af9cf21f57bc36c37135b
Diego Ramírez <diego_rd93@hotmail.com>
2015-08-17 13:30:19 -0300

    Added prioritized topological sort

--- include/djp/graph/topological_sort.hpp
+++ include/djp/graph/topological_sort.hpp
@@ -7,6 +7,7 @@
 #define DJP_GRAPH_TOPOLOGICAL_SORT_HPP
 
 #include <functional> // for std::function
+#include <queue>      // for std::priority_queue
 #include <stdexcept>  // for std::logic_error
 #include <vector>     // for std::vector
 #include <cstddef>    // for std::size_t
@@ -14,6 +15,9 @@
 namespace djp {
 
 /// \brief Sorts the vertices of the given graph topologically.
+///
+/// Uses a DFS-based approach to find a topological sort of the given graph. If
+/// multiple topological sorts exist, any of them is chosen.
 ///
 /// \param g The target graph.
 ///
@@ -24,6 +28,8 @@ namespace djp {
 ///
 /// \par Complexity
 /// <tt>O(V + E)</tt>
+///
+/// \sa prioritized_topological_sort
 ///
 template <typename Graph>
 std::vector<size_t> topological_sort(const Graph &g) {
@@ -52,6 +58,59 @@ std::vector<size_t> topological_sort(const Graph &g) {
       dfs_visit(v);
 
   return list;
+}
+
+/// \brief Sorts the directed graph in topological order and prioritizes
+/// candidate vertices according to the given comparator.
+///
+/// Sorts the vertices of the graph \p g topologically. If multiple vertices can
+/// be chosen in a given moment, the one with the highest priority will be
+/// outputted first.
+///
+/// \param g The target graph.
+///
+/// \param comp The priority comparator, given two vertices \c u and \c v,
+/// <tt>comp(u, v)</tt> must return \c true if \c u has less priority than \c v.
+///
+/// \param[out] output_vertex Each time a new vertex \c v is added to the
+/// topological sort, it will be notified via this paramater by calling
+/// <tt>output_vertex(v)</tt>. This unary function shall be called exactly once
+/// for each vertex in the graph in their respective topological order. If the
+/// number of calls is less than <tt>g.num_vertices()</tt>, it means that \p g
+/// is not a DAG.
+///
+/// \par Complexity
+/// <tt>O(E + V * log(V))</tt>
+///
+/// \sa topological_sort
+///
+template <typename Graph, typename Comp, typename UnaryFunction>
+void prioritized_topological_sort(const Graph &g, Comp comp,
+                                  UnaryFunction output_vertex) {
+  const size_t num_vertices = g.num_vertices();
+  const size_t num_edges = g.num_edges();
+  std::priority_queue<void, std::vector<size_t>, Comp> Q(comp);
+  std::vector<size_t> in_degree(num_vertices);
+
+  for (size_t e = 0; e != num_edges; ++e)
+    ++in_degree[g.target(e)];
+
+  for (size_t v = 0; v != num_vertices; ++v)
+    if (!in_degree[v])
+      Q.push(v);
+
+  while (!Q.empty()) {
+    const size_t src = Q.top();
+    Q.pop();
+    output_vertex(src);
+
+    for (const auto e : g.out_edges(src)) {
+      const auto tgt = g.target(e);
+      --in_degree[tgt];
+      if (!in_degree[tgt])
+        Q.push(tgt);
+    }
+  }
 }
 
 } // end namespace djp
--- test/graph/topological_sort_test.cpp
+++ test/graph/topological_sort_test.cpp
@@ -8,14 +8,16 @@
 
 #include <djp/graph/directed_graph.hpp> // for djp::directed_graph
 
-#include <algorithm> // for std::find
-#include <stdexcept> // for std::logic_error
-#include <vector>    // for std::vector
+#include <algorithm>  // for std::find
+#include <functional> // for std::greater
+#include <stdexcept>  // for std::logic_error
+#include <vector>     // for std::vector
 
 using namespace djp;
+using std::vector;
 
 static void check_toposort(const directed_graph &g) {
-  std::vector<bool> processed(g.num_vertices());
+  vector<bool> processed(g.num_vertices());
   const auto sorted_list = topological_sort(g);
 
   for (const size_t v : sorted_list) {
@@ -83,4 +85,65 @@ TEST(TopologicalSortTest, ThrowsIfNotADag) {
   graph.add_edge(1, 2);
   graph.add_edge(2, 0);
   EXPECT_THROW(topological_sort(graph), std::logic_error);
+}
+
+static vector<size_t>
+smallest_numbered_first_toposort(const directed_graph &g) {
+  vector<size_t> list;
+  list.reserve(g.num_vertices());
+  prioritized_topological_sort(g, std::greater<size_t>(),
+                               [&](size_t v) { list.push_back(v); });
+  return list;
+}
+
+TEST(PrioritizedTopologicalSortTest, EmptyGraphTest) {
+  directed_graph g(0);
+  EXPECT_EQ(vector<size_t>(), smallest_numbered_first_toposort(g));
+}
+
+TEST(PrioritizedTopologicalSortTest, ConnectedDAGTest) {
+  directed_graph g(8);
+  g.add_edge(1, 4);
+  g.add_edge(1, 6);
+  g.add_edge(2, 7);
+  g.add_edge(3, 4);
+  g.add_edge(3, 7);
+  g.add_edge(4, 5);
+  g.add_edge(7, 0);
+  g.add_edge(7, 5);
+  g.add_edge(7, 6);
+
+  ASSERT_EQ(9, g.num_edges());
+  EXPECT_EQ(vector<size_t>({1, 2, 3, 4, 7, 0, 5, 6}),
+            smallest_numbered_first_toposort(g));
+}
+
+TEST(PrioritizedTopologicalSortTest, DisconnectedDAGTest) {
+  directed_graph g(10);
+  g.add_edge(1, 4);
+  g.add_edge(1, 6);
+  g.add_edge(4, 6);
+  g.add_edge(4, 8);
+  g.add_edge(5, 8);
+  g.add_edge(7, 4);
+  g.add_edge(8, 6);
+  g.add_edge(9, 0);
+
+  ASSERT_EQ(8, g.num_edges());
+  EXPECT_EQ(vector<size_t>({1, 2, 3, 5, 7, 4, 8, 6, 9, 0}),
+            smallest_numbered_first_toposort(g));
+}
+
+TEST(PrioritizedTopologicalSortTest, NotDagTest) {
+  directed_graph g(8);
+  g.add_edge(0, 1);
+  g.add_edge(1, 2);
+  g.add_edge(1, 3);
+
+  g.add_edge(4, 5);
+  g.add_edge(5, 6);
+  g.add_edge(6, 7);
+  g.add_edge(7, 4);
+
+  EXPECT_GT(g.num_vertices(), smallest_numbered_first_toposort(g).size());
 }
