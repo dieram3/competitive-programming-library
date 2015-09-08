55af2f59727829658e3ab3ecf84fb5eaf0cd31ed
Diego Ramírez <diego_rd93@hotmail.com>
2015-09-08 00:50:45 -0300

    Added extended euclidean algorithm

---
+++ include/djp/number_theory/diophantine.hpp
@@ -0,0 +1,51 @@
+//          Copyright Diego Ramírez September 2015
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef DJP_NUMBER_THEORY_DIOPHANTINE_HPP
+#define DJP_NUMBER_THEORY_DIOPHANTINE_HPP
+
+#include <tuple> // For std::tuple, std::make_tuple
+
+namespace djp {
+
+/// \brief Finds the greatest common divisor of two integers besides a pair of
+/// Bézout coefficients.
+///
+/// Uses the extended euclidean algorithm to find a pair of Bézout coefficients
+/// for \c a and \p b, that is, a pair of integers <tt>(x, y)</tt> such that:
+/// <blockquote><tt>a*x + b*y = gcd(a, b)</tt></blockquote>
+///
+/// Bézout identity guarantees that a solution exists for any pair (a, b),
+/// provided that either \p a or \p b is a non-zero integer.
+///
+/// \param a The first input integer.
+/// \param b The second input integer.
+///
+/// \pre <tt>a * b != 0</tt>, that is, either \p a or \p b shall be a non-zero
+/// integer.
+///
+/// \returns An \c std::tuple containing <tt>(d, x, y)</tt>, where <tt>d =
+/// gcd(a, b)</tt> and <tt>(x, y)</tt> are the found Bézout coefficients for
+/// <tt>(a, b)</tt>.
+///
+template <typename T>
+std::tuple<T, T, T> extended_euclid(const T a, const T b) {
+  T r = b, old_r = a;
+  T s = 0, old_s = 1;
+  T t = 1, old_t = 0;
+  while (r) {
+    const T quotient = old_r / r;
+    T tmp;
+    tmp = r, r = old_r - quotient * r, old_r = tmp;
+    tmp = s, s = old_s - quotient * s, old_s = tmp;
+    tmp = t, t = old_t - quotient * t, old_t = tmp;
+  }
+  return old_r > 0 ? std::make_tuple(old_r, old_s, old_t)
+                   : std::make_tuple(-old_r, -old_s, -old_t);
+}
+
+} // end namespace djp
+
+#endif // Header guard
---
+++ test/number_theory/diophantine_test.cpp
@@ -0,0 +1,83 @@
+//          Copyright Diego Ramírez September 2015
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <djp/number_theory/diophantine.hpp>
+#include <gtest/gtest.h>
+
+#include <tuple> // For std::tuple
+
+using namespace djp;
+using triple = std::tuple<long, long, long>;
+
+TEST(ExtendedEuclidTest, InputWithZeroTest) {
+  // Non-zero argument is positive
+  EXPECT_EQ(triple(1, 0, 1), extended_euclid(0, 1));
+  EXPECT_EQ(triple(5, 0, 1), extended_euclid(0, 5));
+  EXPECT_EQ(triple(1, 1, 0), extended_euclid(1, 0));
+  EXPECT_EQ(triple(8, 1, 0), extended_euclid(8, 0));
+
+  // Non-zero argument is negative
+  EXPECT_EQ(triple(1, 0, -1), extended_euclid(0, -1));
+  EXPECT_EQ(triple(6, 0, -1), extended_euclid(0, -6));
+  EXPECT_EQ(triple(1, -1, 0), extended_euclid(-1, 0));
+  EXPECT_EQ(triple(11, -1, 0), extended_euclid(-11, 0));
+}
+
+TEST(ExtendedEuclidTest, PositiveInputTest) {
+  EXPECT_EQ(triple(1, 0, 1), extended_euclid(1, 1));
+  EXPECT_EQ(triple(1, 29, -41), extended_euclid(140, 99));
+  EXPECT_EQ(triple(1, 21718, -4237), extended_euclid(9102, 46655));
+
+  EXPECT_EQ(triple(5, -3, 5), extended_euclid(65, 40));
+  EXPECT_EQ(triple(2, -9, 47), extended_euclid(240, 46));
+  EXPECT_EQ(triple(4, 8, -107), extended_euclid(1124, 84));
+  EXPECT_EQ(triple(21, -16, 27), extended_euclid(1239, 735));
+  EXPECT_EQ(triple(30, -7, 3), extended_euclid(420, 990));
+  EXPECT_EQ(triple(33, -9, 1), extended_euclid(693, 6270));
+  EXPECT_EQ(triple(2431, -5, 72), extended_euclid(454597, 31603));
+  EXPECT_EQ(triple(1961, -419, 130), extended_euclid(3296441, 10624698));
+}
+
+TEST(ExtendedEuclidTest, NegativeInputTest) {
+  EXPECT_EQ(triple(1, 0, -1), extended_euclid(-1, -1));
+  EXPECT_EQ(triple(1, 1, -2), extended_euclid(-7, -4));
+  EXPECT_EQ(triple(1, -5041, 14441), extended_euclid(-52788, -18427));
+
+  EXPECT_EQ(triple(9, -1, 0), extended_euclid(-9, -27));
+  EXPECT_EQ(triple(4, 0, -1), extended_euclid(-8, -4));
+  EXPECT_EQ(triple(3, 1, -1), extended_euclid(-12, -15));
+  EXPECT_EQ(triple(6, -22, 5), extended_euclid(-78, -342));
+  EXPECT_EQ(triple(2, -563, 67), extended_euclid(-412, -3462));
+  EXPECT_EQ(triple(2, -179, 708), extended_euclid(-5134, -1298));
+  EXPECT_EQ(triple(1010, 1, -41), extended_euclid(-206040, -5050));
+  EXPECT_EQ(triple(287, 487, -200), extended_euclid(-222999, -543004));
+}
+
+TEST(ExtendedEuclidTest, PositiveAndNegativeInputTest) {
+  EXPECT_EQ(triple(1, 0, -1), extended_euclid(1, -1));
+  EXPECT_EQ(triple(1, -10, -7), extended_euclid(44, -63));
+  EXPECT_EQ(triple(1, 23794, 13073), extended_euclid(32110, -58443));
+
+  EXPECT_EQ(triple(12, 2, 1), extended_euclid(36, -60));
+  EXPECT_EQ(triple(7, -1, -3), extended_euclid(98, -35));
+  EXPECT_EQ(triple(13, 1, 1), extended_euclid(130, -117));
+  EXPECT_EQ(triple(125, 0, -1), extended_euclid(875, -125));
+  EXPECT_EQ(triple(216, 1, 0), extended_euclid(216, -2160));
+  EXPECT_EQ(triple(987, 17, 12), extended_euclid(99687, -141141));
+  EXPECT_EQ(triple(11, 13381, 296), extended_euclid(109263, -4939352));
+}
+
+TEST(ExtendedEuclidTest, NegativeAndPositiveInputTest) {
+  EXPECT_EQ(triple(1, 0, 1), extended_euclid(-1, 1));
+  EXPECT_EQ(triple(1, 3, 4), extended_euclid(-121, 91));
+  EXPECT_EQ(triple(1, -291, -365), extended_euclid(-1386, 1105));
+
+  EXPECT_EQ(triple(10, -16, -9), extended_euclid(-220, 390));
+  EXPECT_EQ(triple(10, 0, 1), extended_euclid(-30, 10));
+  EXPECT_EQ(triple(15, -1, 0), extended_euclid(-15, 105));
+  EXPECT_EQ(triple(33, 1, 1), extended_euclid(-693, 726));
+  EXPECT_EQ(triple(221, 16, 1), extended_euclid(-2873, 46189));
+  EXPECT_EQ(triple(76, 4, 5), extended_euclid(-1976, 1596));
+}
