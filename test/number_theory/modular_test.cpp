48225906397e2ef1febc8a3a31a6445592d13646
Diego Ramírez <diego_rd93@hotmail.com>
2015-09-09 20:30:08 -0300

    Refactor: Use extended euclid theorem in modular inverse

--- test/number_theory/modular_test.cpp
+++ test/number_theory/modular_test.cpp
@@ -9,7 +9,8 @@
 #include <djp/utility/basics.hpp> // for djp::repeat
 
 #include <random>  // for mt19937, uniform_int_distribution
-#include <cstdint> // for uint32_t, SIZE_MAX, INT64_MAX
+#include <cstdint> // for uint32_t, uint64_t, SIZE_MAX, INT64_MAX
+#include <cstdlib> // for std::abs
 
 using namespace djp;
 
@@ -65,27 +66,135 @@ TEST(ModPowTest, WorksWhenOverflowDoesNotNeedControl) {
   EXPECT_EQ(370, mod_pow<size_t>(292, SIZE_MAX, 41202));
 }
 
-TEST(ModInverseTest, WorksWithPrimeModules) {
+namespace {
 
-  EXPECT_EQ(1, mod_inverse(1, 2));
+class ModInverseTest : public ::testing::Test {
+protected:
+  using int_t = std::int32_t;
 
-  EXPECT_EQ(2, mod_inverse(2, 3));
+private:
+  template <typename T>
+  int_t normalize(T x) const {
+    x %= mod;
+    return static_cast<int_t>(x < 0 ? x + mod : x);
+  }
+  static int_t gcd(int_t a, int_t b) {
+    int_t tmp;
+    while (b)
+      tmp = b, b = a % b, a = tmp;
+    return std::abs(a);
+  }
 
-  EXPECT_EQ(2, mod_inverse(4, 7));
-  EXPECT_EQ(4, mod_inverse(2, 7));
+protected:
+  void set_mod(int_t m) {
+    assert(m > 0);
+    mod = m;
+  }
+  void check_inverses(int_t x, int_t y) const {
+    ASSERT_EQ(1, gcd(x, mod));
+    ASSERT_EQ(1, gcd(y, mod));
+    const int_t inv_x = mod_inverse(x, mod);
+    const int_t inv_y = mod_inverse(y, mod);
+    EXPECT_EQ(normalize(y), inv_x);
+    EXPECT_EQ(normalize(x), inv_y);
+    EXPECT_EQ(int_t(1) % mod, normalize(std::int64_t(x) * y));
+  }
+  void check_non_invertible(int_t x) const {
+    ASSERT_NE(1, gcd(x, mod));
+    EXPECT_EQ(mod, mod_inverse(x, mod));
+  }
 
-  EXPECT_EQ(8, mod_inverse(6, 47));
-  EXPECT_EQ(6, mod_inverse(8, 47));
+private:
+  int_t mod = 11;
+};
 
-  EXPECT_EQ(22, mod_inverse(17, 373));
-  EXPECT_EQ(17, mod_inverse(22, 373));
+} // end anonymous namespace
 
-  EXPECT_EQ(144, mod_inverse(55, 7919));
-  EXPECT_EQ(55, mod_inverse(144, 7919));
+TEST_F(ModInverseTest, PrimeModulesTest) {
+  set_mod(2), check_inverses(1, 1);
+  set_mod(3), check_inverses(2, 2);
+  set_mod(7), check_inverses(2, 4);
+  set_mod(47), check_inverses(6, 8);
+  set_mod(373), check_inverses(17, 22);
+  set_mod(7919), check_inverses(55, 144);
+  set_mod(1000000007), check_inverses(504, 1984127);
+  set_mod(2147483647), check_inverses(32768, 65536);
+}
 
-  EXPECT_EQ(1984127, mod_inverse(504, 1000000007));
-  EXPECT_EQ(504, mod_inverse(1984127, 1000000007));
+TEST_F(ModInverseTest, NonPrimeModulesTest) {
+  set_mod(1), check_inverses(0, 0);
+  set_mod(4), check_inverses(1, 1);
+  set_mod(8), check_inverses(3, 3);
+  set_mod(9), check_inverses(2, 5);
 
-  EXPECT_EQ(32768, mod_inverse(65536, 2147483647));
-  EXPECT_EQ(65536, mod_inverse(32768, 2147483647));
+  set_mod(63);
+  check_inverses(1, 1);
+  check_inverses(2, 32);
+  check_inverses(4, 16);
+  check_inverses(8, 8);
+  check_inverses(1, 64);
+  check_inverses(1, 190);
+  check_inverses(2, 95);
+  check_inverses(1, 316);
+  check_inverses(2, 158);
+  check_inverses(4, 79);
+
+  // 585,690,336 = 2^5 * 3^2* 7^5 * 11^2
+  // 585,690,335 = 5 * 617 * 189851
+  set_mod(585690335);
+  check_inverses(1089, 537824);
+  check_inverses(363, 1613472);
+  check_inverses(4312, 135828);
+  check_inverses(12936, 45276);
+  check_inverses(17248, 33957);
+  check_inverses(539, 1086624);
+  check_inverses(3696, 158466);
+  check_inverses(3872, 151263);
+  check_inverses(2, 292845168);
+}
+
+TEST_F(ModInverseTest, OutsideBoundsTest) {
+  // 2118588821 = 683 * 823 * 3769
+  set_mod(2118588821);
+
+  check_non_invertible(0);
+  check_non_invertible(683);
+  check_non_invertible(823);
+  check_non_invertible(3769);
+  check_non_invertible(-393854880);
+  check_non_invertible(-2143446713);
+  check_non_invertible(770251461);
+  check_non_invertible(1599487648);
+
+  check_inverses(-133711905, 1383926620);
+  check_inverses(-372047867, 724363770);
+  check_inverses(-404620562, 362006296);
+  check_inverses(-708632711, 1104498801);
+  check_inverses(-795755684, 229364503);
+  check_inverses(33378331, 581869302);
+  check_inverses(545404204, 778808546);
+  check_inverses(949333985, 958761031);
+
+  // 5201843 = 2017 * 2579
+  set_mod(5201843);
+
+  check_non_invertible(0);
+  check_non_invertible(2017);
+  check_non_invertible(2579);
+  check_non_invertible(56476);
+  check_non_invertible(79941263);
+  check_non_invertible(1407993071);
+  check_non_invertible(-923786);
+  check_non_invertible(-1082713498);
+
+  check_inverses(-795755684, 4601445);
+  check_inverses(4892046, 581869302);
+  check_inverses(-404620562, 4309062);
+  check_inverses(-708632711, 3159960);
+  check_inverses(878686, 545404204);
+  check_inverses(-133711905, 2535877);
+  check_inverses(-372047867, 2692923);
+  check_inverses(1143855, 949333985);
+  check_inverses(-1579004998, 4763700);
+  check_inverses(2615886, 1323567403);
 }
