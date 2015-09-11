28d73d78e4f8c7d92ae9108ee260dcd4011c225d
Diego Ramírez <diego_rd93@hotmail.com>
2015-09-11 03:28:22 -0300

    Use library GCD in modular_test.cpp

--- test/number_theory/modular_test.cpp
+++ test/number_theory/modular_test.cpp
@@ -6,7 +6,8 @@
 #include <djp/number_theory/modular.hpp>
 #include <gtest/gtest.h>
 
-#include <djp/utility/basics.hpp> // for djp::repeat
+#include <djp/number_theory/euclid.hpp> // for djp:gcd
+#include <djp/utility/basics.hpp>       // for djp::repeat
 
 #include <random>  // for mt19937, uniform_int_distribution
 #include <cstdint> // for uint32_t, uint64_t, int32_t, SIZE_MAX, INT64_MAX
@@ -77,12 +78,6 @@ private:
   int_t normalize(T x) const {
     x %= mod;
     return static_cast<int_t>(x < 0 ? x + mod : x);
-  }
-  static int_t gcd(int_t a, int_t b) {
-    int_t tmp;
-    while (b)
-      tmp = b, b = a % b, a = tmp;
-    return std::abs(a);
   }
 
 protected:
