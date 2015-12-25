5dadfcca6cdec90327e47e712794be30ab1bf1b4
Diego Ramírez <dieram3@gmail.com>
2015-12-25 19:33:06 -0300

    Fixed bug in LCP when using null characters. Added better unit tests and removed 'random' module usage

--- test/strings/lcp_array_test.cpp
+++ test/strings/lcp_array_test.cpp
@@ -1,4 +1,4 @@
-//          Copyright Diego Ramírez June 2015
+//          Copyright Diego Ramírez 2015
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -8,75 +8,119 @@
 
 #include <djp/strings/suffix_array.hpp>
 
-#include <algorithm>
-#include <functional>
-#include <iterator>
-#include <random>
-#include <string>
-#include <vector>
-#include <cstddef>
+#include <algorithm> // mismatch
+#include <cassert>   // assert
+#include <cstddef>   // size_t
+#include <iterator>  // begin, distance
+#include <string>    // string
+#include <utility>   // swap
+#include <vector>    // vector
+
+using namespace djp;
+using std::size_t;
+
+// ==========================================
+// MakeLCPArrayTest
+// ==========================================
 
 namespace {
+class MakeLCPArrayTest : public ::testing::Test {
+protected:
+  using lcp_array = std::vector<size_t>;
+  using suffix_array = std::vector<size_t>;
 
-/// \brief Computes the longest common prefix of the suffixes s1 and s2
-/// Complexity: Linear in the size of str.
-static inline size_t naive_lcp(const std::string &str, size_t s1, size_t s2) {
+protected:
+  void check_lcp(const std::string &str, const lcp_array &expected_lcp) {
+    ASSERT_EQ(str.size(), expected_lcp.size());
+    const suffix_array sa = make_suffix_array(str);
+    const lcp_array actual_lcp = make_lcp_array(str, sa);
+    EXPECT_EQ(expected_lcp, actual_lcp) << " bad LCP for string: " << str;
+  }
+};
+} // end anonymous namespace
+
+TEST_F(MakeLCPArrayTest, EmptyStringTest) { check_lcp("", {}); }
+
+TEST_F(MakeLCPArrayTest, OneCharacterTest) {
+  check_lcp("b", {0});
+  check_lcp("x", {0});
+  check_lcp("1", {0});
+  check_lcp(" ", {0});
+  check_lcp(std::string(1, '\0'), {0});
+}
+
+TEST_F(MakeLCPArrayTest, MultipleCharactersTest) {
+  check_lcp("banana", {1, 3, 0, 0, 2, 0});
+  check_lcp("mississippi", {1, 1, 4, 0, 0, 1, 0, 2, 1, 3, 0});
+  check_lcp("alphabetically", {1, 2, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0});
+  check_lcp("loop,loop,loop", {5, 0, 4, 9, 0, 3, 8, 1, 2, 7, 0, 1, 6, 0});
+
+  check_lcp(std::string(5, 'a'), {1, 2, 3, 4, 0});
+  check_lcp(std::string(5, 'z'), {1, 2, 3, 4, 0});
+  check_lcp(std::string(5, ' '), {1, 2, 3, 4, 0});
+  check_lcp(std::string(5, '\0'), {1, 2, 3, 4, 0});
+}
+
+// ==========================================
+// LCPQuerierTest
+// ==========================================
+
+static size_t naive_lcp(const std::string &str, size_t s1, size_t s2) {
+  using std::begin;
+  using std::end;
+  using std::distance;
+
   if (s1 == s2)
     return str.size() - s1;
+  if (s1 < s2)
+    std::swap(s1, s2);
   const auto begin1 = begin(str) + s1;
   const auto begin2 = begin(str) + s2;
-  return std::mismatch(begin1, end(str), begin2).first - begin1;
+  assert(distance(begin1, end(str)) < distance(begin2, end(str)));
+
+  const auto mm_pair = std::mismatch(begin1, end(str), begin2);
+  return distance(begin1, mm_pair.first);
 }
 
-/// \brief Generates a random string with characters uniformly distributed
-///        between [char_min, char_max]
-static inline std::string generate_random_string(std::size_t size,
-                                                 unsigned char char_min = 0,
-                                                 unsigned char char_max = 255) {
-  std::string str(size, '\0');
-  std::mt19937 gen;
-  std::uniform_int_distribution<unsigned char> dist(char_min, char_max);
-  std::generate(begin(str), end(str), std::bind(dist, std::ref(gen)));
-  return str;
-}
+namespace {
+class LCPQuerierTest : public ::testing::Test {
+protected:
+  using suffix_array = std::vector<size_t>;
 
-} // anonymous namespace
+protected:
+  /// \brief Builds and \c lcp_querier an check that every valid pair of
+  /// suffixes (i,j) gives the correct LCP, as computed by \c naive_lcp
+  /// function.
+  ///
+  void check_all_pairs(const std::string &str) {
+    const suffix_array sa = make_suffix_array(str);
+    const lcp_querier get_lcp(str, sa);
 
-TEST(make_lcp_array, WorksOnSmallStrings) {
-  std::string str = "banana";
-  const auto sa = djp::make_suffix_array(str);
-  const auto actual_lcp = djp::make_lcp_array(str, sa);
-  std::vector<size_t> expected_lcp = {1, 3, 0, 0, 2};
-  EXPECT_EQ(expected_lcp, actual_lcp);
-}
-
-TEST(lcp_querier, WorksOnSmallStrings) {
-  std::string str = "abbaabbaab";
-  const auto sa = djp::make_suffix_array(str);
-  const djp::lcp_querier get_lcp(str, sa);
-
-  EXPECT_EQ(6, get_lcp(0, 4));
-  EXPECT_EQ(6, naive_lcp(str, 0, 4));
-
-  // It proves every combination in two ways (a, b) and (b, a)
-  for (size_t i = 0; i < str.size(); ++i)
-    for (size_t j = 0; j < str.size(); ++j)
-      EXPECT_EQ(naive_lcp(str, i, j), get_lcp(i, j)) << i << ' ' << j;
-}
-
-TEST(lcp_querier, WorksOnBigStrings) {
-  const std::string str = generate_random_string(10000, 'A', 'Z');
-  const auto sa = djp::make_suffix_array(str);
-  const djp::lcp_querier get_lcp(str, sa);
-
-  std::default_random_engine gen;
-  std::uniform_int_distribution<size_t> dist(0, str.size() - 1);
-
-  for (size_t i = 0; i < str.size(); ++i) {
-    const size_t s1 = dist(gen);
-    const size_t s2 = dist(gen);
-    get_lcp(s1, s2);
-    // This relies on the uniform distribution of str so no prefix is so long.
-    EXPECT_EQ(naive_lcp(str, s1, s2), get_lcp(s1, s2));
+    for (size_t i = 0; i < str.size(); ++i)
+      for (size_t j = 0; j < str.size(); ++j)
+        EXPECT_EQ(naive_lcp(str, i, j), get_lcp(i, j))
+            << "string=" << str << ", "
+            << "(i,j)=(" << i << "," << j << ")";
   }
+};
+} // end anonymous namespace
+
+TEST_F(LCPQuerierTest, EmptyStringTest) { check_all_pairs(""); }
+
+TEST_F(LCPQuerierTest, OneCharacterTest) {
+  check_all_pairs("a");
+  check_all_pairs("b");
+  check_all_pairs(" ");
+  check_all_pairs(std::string(1, '\0'));
+}
+
+TEST_F(LCPQuerierTest, WorksOnSmallStrings) {
+  check_all_pairs("banana");
+  check_all_pairs("mississippi");
+  check_all_pairs("loop,loop,loop");
+  check_all_pairs("abbaabbaab");
+
+  check_all_pairs(std::string(5, 's'));
+  check_all_pairs(std::string(5, ' '));
+  check_all_pairs(std::string(5, '\0'));
 }
