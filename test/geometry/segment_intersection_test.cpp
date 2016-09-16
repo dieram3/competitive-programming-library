25dfac3964c981f159df6d849c4dac48b5561ab4
Diego <dieram3@gmail.com>
2016-09-15 22:13:11 -0300

    Refactored tests of SimplePolygonTest

--- test/geometry/segment_intersection_test.cpp
+++ test/geometry/segment_intersection_test.cpp
@@ -8,6 +8,8 @@
 
 #include <cpl/geometry/point_2d.hpp> // point
 #include <algorithm>                 // count
+#include <cassert>                   // assert
+#include <initializer_list>          // initializer_list
 #include <utility>                   // make_pair
 #include <vector>                    // vector
 
@@ -338,15 +340,24 @@ protected:
   std::vector<point_t> points;
 
   // Adds a point to the end of the list.
-  void add(scalar_t x, scalar_t y) {
-    points.emplace_back(x, y);
+  void add(point_t point) {
+    points.emplace_back(point);
+  }
+  void add(std::initializer_list<point_t> list) {
+    for (const auto& point : list) {
+      add(point);
+    }
   }
 
   // Adds a point to the end of the list relative to the last point.
-  void rel_add(scalar_t x_delta, scalar_t y_delta) {
-    const auto prev_x = points.back().x;
-    const auto prev_y = points.back().y;
-    add(prev_x + x_delta, prev_y + y_delta);
+  void rel_add(point_t delta) {
+    assert(!points.empty()); // There must be a point of reference.
+    add(points.back() + delta);
+  }
+  void rel_add(std::initializer_list<point_t> deltas) {
+    for (const auto& delta : deltas) {
+      rel_add(delta);
+    }
   }
 
   bool is_simple() {
@@ -363,58 +374,72 @@ protected:
 } // End anonymous namespace
 
 TEST_F(SimplePolygonTest, SquareTest) {
-  add(0, 0);
-  add(0, 2);
-  add(2, 2);
-  add(2, 0);
+  add({
+      {0, 0}, {0, 2}, {2, 2}, {2, 0},
+  });
   EXPECT_TRUE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, CrossingSquaresTest) {
-  add(0, 1), add(1, 1), add(1, 0);
+  add({
+      {0, 1}, {1, 1}, {1, 0},
+  });
   EXPECT_TRUE(is_simple());
-  add(0, 0);
+
+  add({0, 0});
   EXPECT_TRUE(is_simple());
+
   points.pop_back();
-  add(-1, 0);
+  add({-1, 0});
   EXPECT_TRUE(is_simple());
-  add(-1, -1);
+
+  add({-1, -1});
   EXPECT_FALSE(is_simple());
-  add(0, -1);
+
+  add({0, -1});
   EXPECT_FALSE(is_simple());
+
   points.pop_back();
-  add(1, -1);
+  add({1, -1});
   EXPECT_FALSE(is_simple());
+
   points.pop_back();
-  add(2, -1);
+  add({2, -1});
   EXPECT_FALSE(is_simple());
-  add(2, 0);
+
+  add({2, 0});
   EXPECT_FALSE(is_simple());
+
   points.pop_back();
-  add(2, 2);
+  add({2, 2});
   EXPECT_TRUE(is_simple());
-  add(-1, 2);
+
+  add({-1, 2});
   EXPECT_TRUE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, OneIntersectionTest) {
-  add(0, 0);
-  rel_add(3, -2), rel_add(1, 3), rel_add(-2, 1);
-  rel_add(2, -5), rel_add(-2, -1), rel_add(1, 1);
+  add({0, 0});
+  rel_add({
+      {3, -2}, {1, 3}, {-2, 1}, {2, -5}, {-2, -1}, {1, 1},
+  });
   ASSERT_EQ(7, points.size());
   ASSERT_EQ(point_t(3, -3), points.back());
   EXPECT_FALSE(is_simple());
 
   points.clear();
-  add(0, 0);
-  rel_add(1, 3), rel_add(3, 0), rel_add(1, -3);
-  rel_add(-2, -3), rel_add(-2, 0), rel_add(0, 5);
+  add({0, 0});
+  rel_add({
+      {1, 3}, {3, 0}, {1, -3}, {-2, -3}, {-2, 0}, {0, 5},
+  });
   EXPECT_TRUE(is_simple());
-  rel_add(3, 0);
-  rel_add(0, -2);
+
+  rel_add({3, 0});
+  rel_add({0, -2});
   EXPECT_FALSE(is_simple());
-  rel_add(-2, 0);
-  rel_add(0, -1);
+
+  rel_add({-2, 0});
+  rel_add({0, -1});
   EXPECT_FALSE(is_simple());
 
   EXPECT_EQ(point_t(2, -1), points.back());
@@ -422,115 +447,162 @@ TEST_F(SimplePolygonTest, OneIntersectionTest) {
 }
 
 TEST_F(SimplePolygonTest, MultipleIntersecionsTest) {
-  add(0, 0);
-  rel_add(0, 3), rel_add(2, -2), rel_add(0, 2), rel_add(1, -2);
-  rel_add(2, 0), rel_add(-2, -2), rel_add(2, 1), rel_add(-2, -2);
+  add({0, 0});
+  rel_add({
+      {0, 3}, {2, -2}, {0, 2}, {1, -2}, {2, 0}, {-2, -2}, {2, 1}, {-2, -2},
+  });
   EXPECT_TRUE(is_simple());
-  rel_add(-2, 5);
+  rel_add({-2, 5});
   EXPECT_FALSE(is_simple());
-  rel_add(-2, -2);
-  rel_add(2, -1);
+  rel_add({-2, -2});
+  rel_add({2, -1});
   EXPECT_FALSE(is_simple());
 
   EXPECT_EQ(point_t(1, 0), points.back());
   EXPECT_EQ(12, points.size());
 
   points.clear();
-  add(0, 0);
-  rel_add(1, 1), rel_add(1, -1), rel_add(-2, -2), rel_add(2, -2);
-  rel_add(-1, -1), rel_add(-1, 1), rel_add(2, 2);
+  add({0, 0});
+  rel_add({
+      {1, 1}, {1, -1}, {-2, -2}, {2, -2}, {-1, -1}, {-1, 1}, {2, 2},
+  });
   EXPECT_EQ(point_t(2, -2), points.back());
   EXPECT_EQ(8, points.size());
   EXPECT_FALSE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, NoIntersectionTest) {
-  add(0, 0);
-  rel_add(0, 3), rel_add(1, -2), rel_add(1, 1), rel_add(0, -3);
-  rel_add(-1, 1), rel_add(0, -1), rel_add(1, -1), rel_add(1, 1);
-  rel_add(0, 3), rel_add(-1, 1), rel_add(-1, -1), rel_add(0, 1);
-  rel_add(1, 1), rel_add(2, -2), rel_add(0, -3), rel_add(-2, -2);
-  rel_add(-3, 3), rel_add(0, 3);
-
+  add({0, 0});
+  rel_add({
+      {0, 3},
+      {1, -2},
+      {1, 1},
+      {0, -3},
+      {-1, 1},
+      {0, -1},
+      {1, -1},
+      {1, 1},
+      {0, 3},
+      {-1, 1},
+      {-1, -1},
+      {0, 1},
+      {1, 1},
+      {2, -2},
+      {0, -3},
+      {-2, -2},
+      {-3, 3},
+      {0, 3},
+  });
   ASSERT_EQ(19, points.size());
   ASSERT_EQ(point_t(-1, 3), points.back());
   EXPECT_TRUE(is_simple());
 
   points.clear();
-  add(0, 0);
-  rel_add(0, 3), rel_add(1, -2), rel_add(-1, -2), rel_add(-1, 5);
+  add({0, 0});
+  rel_add({
+      {0, 3}, {1, -2}, {-1, -2}, {-1, 5},
+  });
   ASSERT_EQ(5, points.size());
   ASSERT_EQ(point_t(-1, 4), points.back());
   EXPECT_TRUE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, TangentPointTest) {
-  add(0, 0);
-  rel_add(2, 2), rel_add(0, -3), rel_add(2, 2), rel_add(-4, 2);
+  add({0, 0});
+  rel_add({
+      {2, 2}, {0, -3}, {2, 2}, {-4, 2},
+  });
   ASSERT_EQ(point_t(0, 3), points.back());
   ASSERT_EQ(5, points.size());
   EXPECT_FALSE(is_simple());
 
   points.clear();
-  add(0, 0);
-  rel_add(2, 2), rel_add(0, -3), rel_add(2, 3), rel_add(0, -4);
-  rel_add(2, 0), rel_add(0, -1);
+  add({0, 0});
+  rel_add({
+      {2, 2}, {0, -3}, {2, 3}, {0, -4}, {2, 0}, {0, -1},
+  });
   ASSERT_EQ(7, points.size());
   ASSERT_EQ(point_t(6, -3), points.back());
   EXPECT_FALSE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, CollinearChainedTest) {
-  add(0, 0);
-  rel_add(0, 1), rel_add(0, 1), rel_add(1, 1);
-  rel_add(0, -1), rel_add(0, -1), rel_add(0, -1), rel_add(0, -1);
-  rel_add(-1, 0), rel_add(-1, 0);
-  rel_add(0, 1), rel_add(0, 1), rel_add(0, 1);
+  add({0, 0});
+  rel_add({
+      {0, 1},
+      {0, 1},
+      {1, 1},
+      {0, -1},
+      {0, -1},
+      {0, -1},
+      {0, -1},
+      {-1, 0},
+      {-1, 0},
+      {0, 1},
+      {0, 1},
+      {0, 1},
+  });
   ASSERT_EQ(13, points.size());
   ASSERT_EQ(point_t(-1, 2), points.back());
   EXPECT_TRUE(is_simple());
 
   points.clear();
-  add(0, 0);
-  rel_add(1, 1), rel_add(1, 1), rel_add(1, 1), rel_add(1, 1), rel_add(1, 1);
-  rel_add(-4, -5);
+  add({0, 0});
+  rel_add({
+      {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {-4, -5},
+  });
   ASSERT_EQ(7, points.size());
   ASSERT_EQ(point_t(1, 0), points.back());
   EXPECT_TRUE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, OverlappedSegments) {
-  add(0, 0);
-  rel_add(1, 3), rel_add(2, 0), rel_add(1, -3), rel_add(1, 3);
+  add({0, 0});
+  rel_add({
+      {1, 3}, {2, 0}, {1, -3}, {1, 3},
+  });
   ASSERT_EQ(5, points.size());
   ASSERT_EQ(point_t(5, 3), points.back());
   EXPECT_FALSE(is_simple());
 
   points.clear();
-  add(0, 0);
-  rel_add(0, 2), rel_add(1, 0), rel_add(0, -2);
-  rel_add(1, 0), rel_add(0, 3), rel_add(-2, 0);
+  add({0, 0});
+  rel_add({
+      {0, 2}, {1, 0}, {0, -2}, {1, 0}, {0, 3}, {-2, 0},
+  });
   ASSERT_EQ(7, points.size());
   ASSERT_EQ(point_t(0, 3), points.back());
   EXPECT_FALSE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, EightTest) {
-  add(0, 0);
-  rel_add(1, 1), rel_add(1, -1), rel_add(-1, -1), rel_add(-1, -1);
-  rel_add(1, -1), rel_add(1, 1);
-  rel_add(-1, 1);
+  add({0, 0});
+  rel_add({
+      {1, 1}, {1, -1}, {-1, -1}, {-1, -1}, {1, -1}, {1, 1}, {-1, 1},
+  });
   ASSERT_EQ(8, points.size());
   ASSERT_EQ(point_t(1, -1), points.back());
   EXPECT_TRUE(is_simple());
 }
 
 TEST_F(SimplePolygonTest, RareSimplePolygonTest) {
-  add(0, 0);
-  rel_add(3, -4), rel_add(2, 7), rel_add(5, 0), rel_add(-3, -3);
-  rel_add(-3, -4), rel_add(3, -2), rel_add(3, -2), rel_add(1, 0);
-  rel_add(2, -2), rel_add(-3, 2), rel_add(-1, 0), rel_add(-5, -2);
-  rel_add(0, 4), rel_add(-1, -1);
+  add({0, 0});
+  rel_add({
+      {3, -4},
+      {2, 7},
+      {5, 0},
+      {-3, -3},
+      {-3, -4},
+      {3, -2},
+      {3, -2},
+      {1, 0},
+      {2, -2},
+      {-3, 2},
+      {-1, 0},
+      {-5, -2},
+      {0, 4},
+      {-1, -1},
+  });
   ASSERT_EQ(15, points.size());
   ASSERT_EQ(point_t(3, -7), points.back());
   ASSERT_EQ(2, std::count(points.begin(), points.end(), point_t(10, -8)));
