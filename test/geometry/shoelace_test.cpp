7201bc58205341ccf63e2a986c84e715f166982b
Diego <dieram3@gmail.com>
2016-10-30 00:55:02 -0300

    Add compute_polygon_area and done minor tweaks to doxygen comments

--- test/geometry/shoelace_test.cpp
+++ test/geometry/shoelace_test.cpp
@@ -11,6 +11,7 @@
 #include <vector>                    // vector
 
 using cpl::shoelace_area;
+using cpl::compute_polygon_area;
 using point_i = cpl::point2d<int>;
 
 TEST(ShoelaceTest, SimpleShapes) {
@@ -18,14 +19,17 @@ TEST(ShoelaceTest, SimpleShapes) {
   std::vector<point_i> poly = {{0, 0}, {0, 2}, {2, 2}, {2, 0}};
 
   EXPECT_EQ(8, shoelace_area(poly));
+  EXPECT_EQ(4, compute_polygon_area(poly));
 
   // A square with colinear lines
   poly = {{0, 0}, {0, 1}, {0, 2}, {1, 2}, {2, 2}, {2, 1}, {2, 0}, {1, 0}};
   EXPECT_EQ(8, shoelace_area(poly));
+  EXPECT_EQ(4, compute_polygon_area(poly));
 
   // A triangle
   poly = {{2, 6}, {6, 4}, {4, 1}};
   EXPECT_EQ(16, shoelace_area(poly));
+  EXPECT_EQ(8, compute_polygon_area(poly));
 }
 
 TEST(ShoelaceTest, NonConvexPolygon) {
@@ -33,4 +37,5 @@ TEST(ShoelaceTest, NonConvexPolygon) {
                                {3, 2}, {5, 1}, {6, 3}, {10, 3}, {8, 6},
                                {9, 4}, {3, 5}, {4, 6}};
   EXPECT_EQ(42, shoelace_area(poly));
+  EXPECT_EQ(21, compute_polygon_area(poly));
 }
