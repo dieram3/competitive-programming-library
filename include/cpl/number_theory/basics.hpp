230c7975567ee01b25260a43c060161485b7fc9a
Diego Ramírez <dieram3@gmail.com>
2016-12-06 01:24:08 -0300

    Number theory - basics module - minor tweaks
    * Relax ipow requirements
    
    * Make the basics_test consistent
    
    * Make docs consistent

--- include/cpl/number_theory/basics.hpp
+++ include/cpl/number_theory/basics.hpp
@@ -1,14 +1,13 @@
-//          Copyright Diego Ramirez 2014
+//          Copyright Diego Ramirez 2014-2016
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
-/// \file
-/// \brief Defines common functions used in number theory.
 
 #ifndef CPL_NUMBER_THEORY_BASICS_HPP
 #define CPL_NUMBER_THEORY_BASICS_HPP
 
-#include <cassert> // assert
+#include <cassert>     // assert
+#include <type_traits> // is_integral
 
 namespace cpl {
 
@@ -44,36 +43,41 @@ T floor_div(const T x, const T y) {
   return quot - (is_correct ? 0 : 1);
 }
 
-/// \brief Safely checks if <tt>a * b < c</tt>
-/// \pre a, b, c shall not be negative numbers.
+/// \brief Safely checks if `a * b < c`.
+///
+/// \pre `a, b, c` shall not be negative numbers.
+///
 template <typename T>
 constexpr bool multiply_less(T a, T b, T c) {
   return b == 0 ? c > 0 : a < ceil_div(c, b);
 }
 
-/// \brief Safely checks if <tt>a * b > c</tt>
-/// \pre a, b, c shall not be negative numbers.
+/// \brief Safely checks if `a * b > c`.
+///
+/// \pre `a, b, c` shall not be negative numbers.
+///
 template <typename T>
 constexpr bool multiply_greater(T a, T b, T c) {
   return b != 0 && a > c / b;
 }
 
-/// \brief Computes <tt>pow(a, b)</tt> without using floating-point arithmetic.
+/// \brief Computes `pow(a, b)` without using floating-point arithmetic.
 ///
 /// \param base The base.
 /// \param exp The exponent.
 ///
-/// \pre <tt>base >= 0</tt>
-/// \pre <tt>exp >= 0</tt>
+/// \pre `exp >= 0`
 ///
-/// \returns <tt>base</tt> raised to <tt>exp</tt>.
+/// \returns `base` raised to `exp`.
 ///
 /// \par Complexity
-/// Logarithmic in <tt>exp</tt>.
+/// Logarithmic in `exp`.
 ///
-template <typename T>
-T ipow(T base, T exp) {
-  assert(base >= 0 && exp >= 0);
+template <typename T, typename U>
+T ipow(T base, U exp) {
+  static_assert(std::is_integral<U>::value, "");
+  assert(exp >= 0);
+
   T result = 1;
   while (exp > 0) {
     if (exp % 2 == 1)
--- test/number_theory/basics_test.cpp
+++ test/number_theory/basics_test.cpp
@@ -8,13 +8,11 @@
 
 #include <algorithm>        // for_each
 #include <climits>          // INT_MAX
+#include <complex>          // complex
 #include <cstdint>          // uint_fast64_t, UINT64_MAX
 #include <initializer_list> // initializer_list
 #include <tuple>            // tuple, get
 
-using cpl::multiply_less;
-using cpl::multiply_greater;
-using cpl::ipow;
 using std::uint_fast64_t;
 
 namespace {
@@ -107,7 +105,9 @@ TEST_F(BasicsTest, FloorDivTest) {
   EXPECT_EQ(UINT64_MAX, floor_div<uint_fast64_t>(UINT64_MAX, 1));
 }
 
-TEST(MultiplyLessTest, WorksWell) {
+TEST_F(BasicsTest, MultiplyLessTest) {
+  using cpl::multiply_less;
+
   EXPECT_TRUE(multiply_less(0, 0, 1));
   EXPECT_TRUE(multiply_less(0, 1, 1));
   EXPECT_TRUE(multiply_less(1, 0, 1));
@@ -126,7 +126,9 @@ TEST(MultiplyLessTest, WorksWell) {
   EXPECT_FALSE(multiply_less(5, 7, 35));
 }
 
-TEST(MultiplyGreaterTest, WorksWell) {
+TEST_F(BasicsTest, MultiplyGreaterTest) {
+  using cpl::multiply_greater;
+
   EXPECT_TRUE(multiply_greater(65536u, 65536u, 4294967295u));
   EXPECT_TRUE(multiply_greater(4, 3, 10));
   EXPECT_TRUE(multiply_greater(4, 2, 7));
@@ -145,19 +147,43 @@ TEST(MultiplyGreaterTest, WorksWell) {
   EXPECT_FALSE(multiply_greater(3, 3, 10));
 }
 
-TEST(IPowTest, WorksWell) {
-  using int_t = std::uint_fast64_t;
-  EXPECT_EQ(1, ipow<int_t>(1, 0));
-  EXPECT_EQ(1, ipow<int_t>(2, 0));
-  EXPECT_EQ(1, ipow<int_t>(29, 0));
-  EXPECT_EQ(29, ipow<int_t>(29, 1));
-  EXPECT_EQ(841, ipow<int_t>(29, 2));
-  EXPECT_EQ(129140163, ipow<int_t>(3, 17));
-  EXPECT_EQ(476837158203125, ipow<int_t>(5, 21));
-  EXPECT_EQ(819628286980801, ipow<int_t>(31, 10));
+TEST_F(BasicsTest, IPowTest) {
+  using cpl::ipow;
+
+  // Positive base
+  EXPECT_EQ(1, ipow(1, 0));
+  EXPECT_EQ(1, ipow(2, 0));
+  EXPECT_EQ(1, ipow(29, 0));
+  EXPECT_EQ(29, ipow(29, 1));
+  EXPECT_EQ(841, ipow(29, 2));
+  EXPECT_EQ(129140163, ipow(3L, 17));
+  EXPECT_EQ(476837158203125, ipow(5LL, 21));
+  EXPECT_EQ(819628286980801, ipow(31LL, 10));
+
+  // Negative base.
+  EXPECT_EQ(1, ipow(-1, 0));
+  EXPECT_EQ(1, ipow(-2, 0));
+  EXPECT_EQ(1, ipow(-29, 0));
+  EXPECT_EQ(-29, ipow(-29, 1));
+  EXPECT_EQ(841, ipow(-29, 2));
+  EXPECT_EQ(-129140163, ipow(-3L, 17));
+  EXPECT_EQ(-476837158203125, ipow(-5LL, 21));
+  EXPECT_EQ(819628286980801, ipow(-31LL, 10));
 
   // Special cases
   EXPECT_EQ(1, ipow(0, 0));
   EXPECT_EQ(0, ipow(0, 1));
   EXPECT_EQ(0, ipow(0, 2));
+
+  // Floating-point base.
+  EXPECT_DOUBLE_EQ(12.25, ipow(3.5, 2));
+  EXPECT_DOUBLE_EQ(-19349.17632, ipow(-7.2, 5));
+
+  // Complex base.
+  using complex = std::complex<double>;
+  EXPECT_EQ(complex(-1, 0), ipow(complex(0, 1), 2));
+  EXPECT_EQ(complex(1, 0), ipow(complex(0, 1), 4));
+  EXPECT_EQ(complex(-117, 44), ipow(complex(3, 4), 3));
+  EXPECT_EQ(complex(-60422, 116615), ipow(complex(2, 5), 7));
+  EXPECT_EQ(complex(-703919, -68880), ipow(complex(2, 5), 8));
 }
