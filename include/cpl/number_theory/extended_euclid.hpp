0b7006b15759dc5cdeb832bb522c5d6b0259895d
Diego <dieram3@gmail.com>
2016-12-05 22:41:46 -0300

    Minor tweaks and doc fixes to extended_euclid.

--- include/cpl/number_theory/extended_euclid.hpp
+++ include/cpl/number_theory/extended_euclid.hpp
@@ -6,6 +6,7 @@
 #ifndef CPL_NUMBER_THEORY_EXTENDED_EUCLID_HPP
 #define CPL_NUMBER_THEORY_EXTENDED_EUCLID_HPP
 
+#include <cassert>     // assert
 #include <tuple>       // tuple, make_tuple
 #include <type_traits> // is_signed
 
@@ -15,21 +16,21 @@ namespace cpl {
 /// Bézout coefficients.
 ///
 /// Uses the extended euclidean algorithm to find a pair of Bézout coefficients
-/// for \c a and \p b, that is, a pair of integers <tt>(x, y)</tt> such that:
-/// <blockquote><tt>a*x + b*y = gcd(a, b)</tt></blockquote>
-///
-/// Bézout identity guarantees that a solution exists for any pair (a, b),
-/// provided that either \p a or \p b is a non-zero integer.
+/// for `a` and `b`, that is, a pair of integers `(x, y)` such that:
+/// <blockquote>`a*x + b*y = gcd(a, b)`</blockquote>
 ///
 /// \param a The first input integer.
 /// \param b The second input integer.
 ///
-/// \pre <tt>a * b != 0</tt>, that is, either \p a or \p b shall be a non-zero
-/// integer.
+/// \returns A `std::tuple` containing `(d, x, y)`, where `d = gcd(a, b)` and
+/// `(x, y)` are the found Bézout coefficients for `(a, b)`. If both, `a` and
+/// `b` are zero, it returns `(0, x, y)`, where `x` and `y` are arbitrary
+/// integers.
 ///
-/// \returns An \c std::tuple containing <tt>(d, x, y)</tt>, where <tt>d =
-/// gcd(a, b)</tt> and <tt>(x, y)</tt> are the found Bézout coefficients for
-/// <tt>(a, b)</tt>.
+/// \note Bézout identity guarantees that a solution exists for any pair
+/// `(a, b)`, provided that either \p a or \p b is a non-zero integer. However,
+/// if one defines `gcd(0, 0) == 0`, a solution exists for every pair of
+/// integers.
 ///
 template <typename T>
 std::tuple<T, T, T> extended_euclid(const T a, const T b) {
@@ -38,15 +39,15 @@ std::tuple<T, T, T> extended_euclid(const T a, const T b) {
   T r = b, old_r = a;
   T s = 0, old_s = 1;
   T t = 1, old_t = 0;
-  while (r) {
+  while (r != 0) {
     const T quotient = old_r / r;
     T tmp;
     tmp = r, r = old_r - quotient * r, old_r = tmp;
     tmp = s, s = old_s - quotient * s, old_s = tmp;
     tmp = t, t = old_t - quotient * t, old_t = tmp;
   }
-  return old_r > 0 ? std::make_tuple(old_r, old_s, old_t)
-                   : std::make_tuple(-old_r, -old_s, -old_t);
+  return old_r >= 0 ? std::make_tuple(old_r, old_s, old_t)
+                    : std::make_tuple(-old_r, -old_s, -old_t);
 }
 
 } // end namespace cpl
